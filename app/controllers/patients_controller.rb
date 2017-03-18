8846f25d200e18f5310e4e0de5a20d5cf4e60bf7
conorprunty <conorprunty@hotmail.com>
2017-03-18 18:46:32 +0000

    added logging
    each new patient is logged to file

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -29,7 +29,6 @@ class PatientsController < ApplicationController
   # POST /patients.json
   def create
     @patient = Patient.new(patient_params)
-
     respond_to do |format|
       if @patient.save
         format.html { redirect_to @patient, notice: 'Patient was successfully created.' }
