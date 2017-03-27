e1e0e546527271f597c1123c7bef7be5fdbe39b7
conorprunty <conorprunty@hotmail.com>
2017-03-27 20:54:34 +0100

    added admin user
    admin@email.ie / password

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,7 +3,9 @@ require 'date'
 
 class PatientsController < ApplicationController
     #password required to add patient
-    http_basic_authenticate_with name: "test", password: "test", except: [:index, :show]
+    #http_basic_authenticate_with name: "test", password: "test", except: [:index, :show]
+    before_filter :authenticate_user! 
+    before_filter :ensure_admin, :only => [:edit, :destroy] 
 
     
   before_action :set_patient, only: [:show, :edit, :update, :destroy]
@@ -74,6 +76,12 @@ class PatientsController < ApplicationController
       format.json { head :no_content }
     end
   end
+    
+    def ensure_admin
+        unless current_user && current_user.admin?
+            render :text => "Access Error Message", :status => :unauthorized
+        end
+    end
 
   private
     # Use callbacks to share common setup or constraints between actions.
